73507179062cff8d7903f9df864ff1d87125abaf
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2023-10-05 03:02:11 -0700

    Add files via upload

---
+++ WinCustomizerGUI/WinCustomizerGuiDowload.ps1
@@ -0,0 +1,28 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Output "WinCustomizerGUI necessita di essere lanciato come Amministratore. Riavvio."
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm -Uri https://bit.ly/wincustomizergui | iex"
+    break
+}
+
+Set-MpPreference -ExclusionPath $env:USERPROFILE\Desktop\WinCustomizerGUI-main
+
+# Change to the desktop directory
+cd "$env:USERPROFILE\Desktop"
+
+# Download the script from GitHub
+Invoke-WebRequest -Uri "https://github.com/MrNico98/WinCustomizerGUI/archive/refs/heads/main.zip" -OutFile "WinCustomizerGUI-main.zip"
+
+
+Expand-Archive -Path "WinCustomizerGUI-main.zip" -DestinationPath "." -Force
+
+
+Move-Item -Path "WinCustomizerGUI-main\WinCustomizerGUI" -Destination "WinCustomizerGUI" -Force
+
+
+Remove-Item -Path "WinCustomizerGUI-main" -Recurse -Force
+
+
+Remove-Item -Path "WinCustomizerGUI-main.zip" -Force
+
+
+Start-Process -FilePath ".\WinCustomizer\Start.bat"
